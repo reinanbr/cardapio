b6f9504110d3387c9a28d679b204781d8e3a0cf8
reinanbr <perseu912@gmail.com>
2023-04-21 15:10:50 -0300

    stilling

--- static/js/read_info.js
+++ static/js/read_info.js
@@ -239,7 +239,7 @@ const painelHorary = () => {
    // console.log(infoHorary.secondsCount)
     if(!infoHorary.food){
      //   console.log(infoHorary.secondsCount)
-        $('#infoHorary').html(`<small>O lanche será liberado às ${infoHorary.horaryFood.hour}:${infoHorary.horaryFood.minutes}.<br/>Faltam ${infoHorary.timeRes.hour} horas, ${infoHorary.timeRes.minutes} minutos e ${infoHorary.timeRes.seconds} segundos</small>`);
+        $('#infoHorary').html(`<small><b>O lanche será liberado às ${infoHorary.horaryFood.hour}:${infoHorary.horaryFood.minutes}!</b><br/><br/>Faltam ${infoHorary.timeRes.hour} horas, ${infoHorary.timeRes.minutes} minutos e ${infoHorary.timeRes.seconds} segundos</small>`);
     }
 }
 
