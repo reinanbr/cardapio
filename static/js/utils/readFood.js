bd1ea125d61d40b1bca7b31e5001168f8ac7430b
reinanbr <perseu912@gmail.com>
2023-10-09 12:17:36 -0300

    added privacity security pt. 2

--- static/js/utils/readFood.js
+++ static/js/utils/readFood.js
@@ -1,46 +1,45 @@
 // import { readEmojis } from "./readEmojis.js";
 
-import { datetime,monthsName } from "./makeBaseDate.js";
+import { datetime, monthsName } from "./makeBaseDate.js";
 
 let dt = datetime()
-export const readFood = () =>{
+export const readFood = () => {
     fetch('https://reysofts.com.br/apis/cardapio/read_food.php')
-    .then((res)=>{
-       
-        return res.json();
-    })
-    .then((json)=>{
-        //console.log(Object.values(json)[0])
-        let foodValues = Object.values(json)
-        let foodKeys = Object.keys(json)
+        .then((res) => {
 
-        let lastFood = foodValues[foodValues.length-1]
-        let lastFoodKey = lastFood.foodKey
+            return res.json();
+        })
+        .then((json) => {
+            //console.log(Object.values(json)[0])
+            let foodValues = Object.values(json)
+            let foodKeys = Object.keys(json)
 
-        // readEmojis(lastFoodKey)
-        console.log(lastFood)
+            let lastFood = foodValues[foodValues.length - 1]
+            let lastFoodKey = lastFood.foodKey
 
-        $('#foodName').text(lastFood.foodName)
+            // readEmojis(lastFoodKey)
+            console.log(lastFood)
 
-
-        let date = `${dt.day} ${monthsName[dt.month]} ${dt.year}`
-        let contentDescriptionSite = `O lanche do dia ${date} é: ${lastFood.foodName}`
-
-        console.log(contentDescriptionSite)
-        $('meta[name="description"]').attr('content', contentDescriptionSite);
-        $('meta[name="og:description"]').attr('content', contentDescriptionSite);
-        document.head.querySelector('meta[name="description"]').content = contentDescriptionSite
-        document.head.querySelector('meta[name="og:description"]').content = contentDescriptionSite
+            $('#foodName').text(lastFood.foodName)
 
 
-        $('#updateDateFood').text(`Cardápio atualizado às ${lastFood.hora-3}:${lastFood.minuto}, do dia ${lastFood.dia} ${monthsName[lastFood.mes-1]} ${lastFood.ano}.`)
-        $('#sja').text('Sujeito a alterações.')
-    })
-    .catch((e)=>{
-        $('#errorInfo').html(`
+            let date = `${dt.day} ${monthsName[dt.month]} ${dt.year}`
+            let contentDescriptionSite = `O lanche do dia ${date} é: ${lastFood.foodName}`
+
+            console.log(contentDescriptionSite)
+            $('meta[name="description"]').attr('content', contentDescriptionSite);
+            $('meta[name="og:description"]').attr('content', contentDescriptionSite);
+            $("head").append(`<meta property="og:description" content="${contentDescriptionSite}">`);
+ 
+
+            $('#updateDateFood').text(`Cardápio atualizado às ${lastFood.hora - 3}:${lastFood.minuto}, do dia ${lastFood.dia} ${monthsName[lastFood.mes - 1]} ${lastFood.ano}.`)
+            $('#sja').text('Sujeito a alterações.')
+        })
+        .catch((e) => {
+            $('#errorInfo').html(`
         <div class="alert alert-danger">
           <strong>ServerError!</strong> ${e}
         </div>`);
-    })
+        })
 }
 
